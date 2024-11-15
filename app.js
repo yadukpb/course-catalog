e63fa09b310a131b99cd3a9370b49356455908b7
yadukpb1 <yadukrishnapb1@gmail.com>
2024-11-15 23:04:49 +0530

    new deploymnet

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(helmet());
 app.use(compression());
 
 const corsOptions = {
-  origin: process.env.ALLOWED_ORIGINS?.split(',') || ['https://yourdomain.com'],
+  origin: process.env.ALLOWED_ORIGINS?.split(',') || ['https://cloud-project-gj0kixszl-yadukrishna-pbs-projects.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
